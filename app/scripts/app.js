4b53db79e5f91756115998f0389b764cbd876624
seaman <semen.bko@ya.ru>
2015-01-22 22:04:58 +0300

    create some classes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,7 @@
 /** @jsx React.DOM */
 var React = require('react');
-var converter = new Showdown.converter();
 
-var data = [
+var PRODUCTS = [
   {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
   {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
   {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
@@ -11,6 +10,27 @@ var data = [
   {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
 ];
 
+var ProductCategoryRow = React.createClass({
+  render: function(){
+    return (<tr><th colSpan="2">{this.props.category}</th></tr>);
+  }
+})
+
+var ProductRow = React.createClass({
+  render: function(){
+    var name = this.props.stocked ?
+    this.props.product.name :
+    <span style={{color: 'red'}}>
+      {this.props.product.name}
+    </span>;
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{this.props.product.price}</td>
+      </tr>
+      );
+  }
+})
 var ProductTable = React.createClass({
   render: function(){
     var rows = [];
@@ -35,3 +55,28 @@ var ProductTable = React.createClass({
       );
   }
 });
+
+var SearchBar = React.createClass({
+  render: function(){
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+        <p>
+          <input type="checkbox" />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+      )
+  }
+})
+
+var FilterableProductTable = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <SearchBar />
+        <ProductTable products={this.props.products} />
+      )
+  }
+})

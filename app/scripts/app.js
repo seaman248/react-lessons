a3f0bd8ec4193842d77eb650dd932d9c9833cf55
seaman <semen.bko@ya.ru>
2015-01-22 21:40:00 +0300

    producttable class

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,37 @@
 /** @jsx React.DOM */
 var React = require('react');
 var converter = new Showdown.converter();
+
+var data = [
+  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+];
+
+var ProductTable = React.createClass({
+  render: function(){
+    var rows = [];
+    var lastCategory = null;
+    this.props.products.forEach(function(){
+      if (product.category !== lastCategory){
+        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      lastCategory = product.category;
+    });
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+               <th>Price</th>
+            </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+        </table>
+      );
+  }
+});

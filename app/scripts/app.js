54bf2dc971fd8cb31715f88bf9bb116fddcee9a6
seaman <semen.bko@ya.ru>
2015-01-22 23:48:36 +0300

    map

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,5 @@
 /** @jsx React.DOM */
 var React = require('react');
-
 var PRODUCTS = [
   {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
   {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
@@ -36,13 +35,12 @@ var ProductTable = React.createClass({
     render: function() {
         var rows = [];
         var lastCategory = null;
-        this.props.products.forEach(function(product) {
+        this.props.products.map(function(product) {
             if (product.category !== lastCategory) {
                 rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
             }
             rows.push(<ProductRow product={product} key={product.name} />);
             lastCategory = product.category;
-            
         });
         return (
             <table>
